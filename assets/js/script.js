0ec22e6a0358f564816cbc6ba38136be82b3dced
Victor <victormnuzzi003@gmail.com>
2023-07-06 17:34:19 -0300

    adding the option background color change when it is selected in the selectedRating function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 
+// Rating
+let selectedNumber;
+function selectedRating(number) {
+    const selectedCircle = document.getElementById(`rating-circle${number}`).style.backgroundColor;
+;
+    if (selectedNumber == undefined) {
+        selectedNumber = number;
+        document.getElementById(`rating-circle${number}`).style.backgroundColor = "hsl(217, 12%, 63%)";
+        document.getElementById(`number${number}`).style.color = "white";
+    }
+
+
+}
 
 // Submit button
+
 function submitRating() {
     document.getElementById("rating-state").style.display = "none";
     document.getElementById("ty-state").style.display = "flex";
